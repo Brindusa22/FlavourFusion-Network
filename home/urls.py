1c38026cae0b23fcd6540d086772a2bffdc76a49
Brindusa22 Manu <brindusa.manu@gmail.com>
2024-03-25 11:19:22 +0000

    Create breakfast_detail view and urls; wire it up to index template

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
+    path('breakfast/', views.breakfast_detail, name='breakfast_detail')
 ]
--- home/views.py
+++ home/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render, get_object_or_404, reverse
 from django.shortcuts import render
 from django.views import generic
 from recipes.models import Recipe
@@ -10,4 +11,18 @@ class HomePageView(generic.ListView):
     template_name = "home/home_page.html"
 
 
-    
+def breakfast_detail(request):
+    """
+     Displays a list of breakfast recipes.
+     Retrieves all the recipes from the database and filters them to render only those with status 1 and meal_type breakfast.
+     Ordres the rendered recipes by their creation date, in descending order "
+    """
+
+    recipes = Recipe.objects.filter(status=1, meal_type='breakfast').order_by('-created_on')
+     
+   
+    return render(
+        request,
+        "recipes/index.html",
+        {'recipe_list': recipes}
+    )
