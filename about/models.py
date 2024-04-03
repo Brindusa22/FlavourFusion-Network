2cdbf0b399b6960c327432aa906fba82d85e8d6b
Brindusa22 Manu <brindusa.manu@gmail.com>
2024-04-03 13:28:11 +0000

    Install Cloudinary and update about model and template

--- about/models.py
+++ about/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 
@@ -7,6 +9,7 @@ class About(models.Model):
     content = models.TextField()
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
+    profile_image = CloudinaryField('image', default='placeholder')
 
     def __str__ (self):
         return f"{self.name} | last updated on  {self.date_updated}"
