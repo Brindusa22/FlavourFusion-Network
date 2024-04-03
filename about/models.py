03ebccc87d57f53eee38a006d632a907fd1f893e
Brindusa22 Manu <brindusa.manu@gmail.com>
2024-04-03 09:30:44 +0000

    Update about model and template

--- about/models.py
+++ about/models.py
@@ -5,8 +5,8 @@ from django.db import models
 class About(models.Model):
     name = models.CharField(max_length=50)
     content = models.TextField()
-    date_created = models.DateField(auto_now_add=True)
-    date_updated = models.DateField(auto_now=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
 
     def __str__ (self):
         return f"{self.name} | last updated on  {self.date_updated}"
