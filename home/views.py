55b59b0052b159d7f062c5fd0390e40c151c003a
Brindusa22 Manu <brindusa.manu@gmail.com>
2024-04-04 21:27:28 +0000

    Add context var to the meal_type views/home to pass to recepies_list template

--- home/views.py
+++ home/views.py
@@ -30,6 +30,22 @@ class BreakfastList(generic.ListView):
     paginate_by = 6 
     context_object_name= 'recipes'
 
+    def get_context_data(self, **kwargs):
+        """
+        Gets the values of the meal_type = 'breakfast' from the database and insert the values into a list, to have it into a human
+        readable format.
+        Retrieve the first result and assign it to a context variable to pass to the recipe_list template for the <h2> title"
+    
+        """
+
+        context = super().get_context_data(**kwargs)
+        meal_types =list( Recipe.objects.filter(status=1, meal_type='breakfast').values_list('meal_type', flat=True)  )
+        context['category'] = (meal_types[0])
+  
+        
+        return context
+
+
 class MainCourseList(generic.ListView):
     """
     Displays a list of main course recipes.
@@ -42,6 +58,22 @@ class MainCourseList(generic.ListView):
     paginate_by = 6 
     context_object_name= 'recipes'
 
+
+    def get_context_data(self, **kwargs):
+        """
+        Gets the values of the meal_type = 'main_course' from the database and insert the values into a list, to have it into a human
+        readable format.
+        Retrieve the first result and assign it to a context variable to pass to the recipe_list template for the <h2> title"
+    
+        """
+
+        context = super().get_context_data(**kwargs)
+        meal_types =list( Recipe.objects.filter(status=1, meal_type='main_course').values_list('meal_type', flat=True)  )
+        context['category'] = (meal_types[0])
+  
+
+        return context
+
 class SoupList(generic.ListView):
     """
     Displays a list of soup recipes.
@@ -52,7 +84,22 @@ class SoupList(generic.ListView):
     template_name = "recipes/recipes_list.html"
     queryset = Recipe.objects.filter(status=1, meal_type='soup').order_by('title')
     paginate_by = 6
-    context_object_name= 'recipes' 
+    context_object_name= 'recipes'
+
+    def get_context_data(self, **kwargs):
+        """
+        Gets the values of the meal_type = 'soup' from the database and insert the values into a list, to have it into a human
+        readable format.
+        Retrieve the first result and assign it to a context variable to pass to the recipe_list template for the <h2> title"
+    
+        """
+
+        context = super().get_context_data(**kwargs)
+        meal_types =list( Recipe.objects.filter(status=1, meal_type='soup').values_list('meal_type', flat=True)  )
+        context['category'] = (meal_types[0])
+  
+        
+        return context 
 
 
 class AppetizerList(generic.ListView):
@@ -67,6 +114,22 @@ class AppetizerList(generic.ListView):
     paginate_by = 6 
     context_object_name= 'recipes'
 
+    def get_context_data(self, **kwargs):
+        """
+        Gets the values of the meal_type = 'appetizer' from the database and insert the values into a list, to have it into a human
+        readable format.
+        Retrieve the first result and assign it to a context variable to pass to the recipe_list template for the <h2> title"
+    
+        """
+
+        context = super().get_context_data(**kwargs)
+        meal_types =list( Recipe.objects.filter(status=1, meal_type='appetizer').values_list('meal_type', flat=True)  )
+        context['category'] = (meal_types[0])
+  
+        
+        return context
+
+
 class SidesList(generic.ListView):
     """
     Displays a list of sides recipes.
@@ -79,6 +142,20 @@ class SidesList(generic.ListView):
     paginate_by = 6 
     context_object_name= 'recipes'
 
+    def get_context_data(self, **kwargs):
+        """
+        Gets the values of the meal_type = 'sides' from the database and insert the values into a list, to have it into a human
+        readable format.
+        Retrieve the first result and assign it to a context variable to pass to the recipe_list template for the <h2> title"
+    
+        """
+        context = super().get_context_data(**kwargs)
+        meal_types =list( Recipe.objects.filter(status=1, meal_type='sides').values_list('meal_type', flat=True))
+        context['category'] = (meal_types[0])
+  
+        
+        return context
+
 class DessertList(generic.ListView):
     """
     Displays a list of dessert recipes.
@@ -90,6 +167,22 @@ class DessertList(generic.ListView):
     queryset = Recipe.objects.filter(status=1, meal_type='dessert').order_by('title')
     paginate_by = 6 
     context_object_name= 'recipes'
+
+    def get_context_data(self, **kwargs):
+        """
+        Gets the values of the meal_type = 'dessert' from the database and insert the values into a list, to have it into a human
+        readable format.
+        Retrieve the first result and assign it to a context variable to pass to the recipe_list template for the <h2> title"
+    
+        """
+
+        context = super().get_context_data(**kwargs)
+        meal_types =list( Recipe.objects.filter(status=1, meal_type='dessert').values_list('meal_type', flat=True)  )
+        context['category'] = (meal_types[0])
+  
+        
+        return context
+
 
 def search_form(request):
     """
@@ -114,10 +207,6 @@ def search_form(request):
         )
 
 
-class CuisineList(generic.ListView):
 
-    model = Recipe
-    template_name = 'home.html'
-    cuisines = Recipe.objects.values_list('cuisine', flat=True).distinct()
 
    
